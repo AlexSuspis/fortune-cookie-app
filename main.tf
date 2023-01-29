a8a35e26ca5f241cce62b5a8ecdbe1e84a49600c
Alex Suspiro <alexandresuspiro@gmail.com>
2023-01-29 21:00:05 +0000

    comment out key_name referring to ssh key which was removed in the previous commit

--- main.tf
+++ main.tf
@@ -43,9 +43,9 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.generated_key.key_name
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  #   key_name               = aws_key_pair.generated_key.key_name
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
