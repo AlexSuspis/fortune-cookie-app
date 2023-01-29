65bb51f3a45a5d4e8fb6a9ae8c31738782f19ba8
Alex Suspiro <alexandresuspiro@gmail.com>
2023-01-29 14:31:09 +0000

    remove unused key_name variable

--- main.tf
+++ main.tf
@@ -27,13 +27,12 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 # Key generation
-variable "key_name" {}
 resource "tls_private_key" "key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 resource "aws_key_pair" "generated_key" {
-  key_name   = var.key_name
+  key_name   = "key_name"
   public_key = tls_private_key.key.public_key_openssh
 }
 
