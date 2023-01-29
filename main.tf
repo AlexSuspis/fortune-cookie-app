249bdc26833373d1e21cc438d8a0fccc6963b0a3
Alex Suspiro <alexandresuspiro@gmail.com>
2023-01-29 21:18:15 +0000

    add AutoScaling Group and Elastic Balancer to .tf file

--- main.tf
+++ main.tf
@@ -56,20 +56,8 @@ resource "aws_instance" "web" {
                 echo "<h1>Fortune cookie app coming soon!</h1>" > /var/www/html/index.html
                 systemctl restart apache2
                 EOF
-
-  #   connection {
-  #     type = "ssh"
-  #     user = "ubuntu"
-  #     # private_key = file("${path.module}/id_rsa")
-  #     private_key = tls_private_key.example.private_key_pem
-  #     host        = self.public_ip
-  #   }
-  #   provisioner "file" {
-  #     # source      = "/Users/alex/iCloud/coding/fortune-cookie-app/index.html"
-  #     source      = "index.html"
-  #     destination = "/var/www/html/index.html"
-  #   }
 }
+
 
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
@@ -87,6 +75,71 @@ resource "aws_security_group" "web-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+## Creating Launch Configuration
+resource "aws_launch_configuration" "example" {
+  image_id        = lookup(var.amis, var.region)
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
+  key_name        = var.key_name
+  user_data       = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+} ## Creating AutoScaling Group
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.id
+  availability_zones   = ["${data.aws_availability_zones.all.names}"]
+  min_size             = 1
+  max_size             = 5
+  load_balancers       = ["${aws_elb.example.name}"]
+  health_check_type    = "ELB"
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+} ## Security Group for ELB
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+} ### Creating ELB
+resource "aws_elb" "example" {
+  name               = "terraform-asg-example"
+  security_groups    = ["${aws_security_group.elb.id}"]
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:8080/"
+  }
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = "8080"
+    instance_protocol = "http"
+  }
+}
+
+
+
 
 output "web-address" {
   value = "${aws_instance.web.public_dns}:8080"
