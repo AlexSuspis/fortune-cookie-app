3479bca001c6f56e808c9f97c263aebb964fc285
Alex Suspiro <alexandresuspiro@gmail.com>
2023-02-01 01:19:05 +0000

    add key_name to aws_launch_template so ssh connection can take place

--- main.tf
+++ main.tf
@@ -52,28 +52,10 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  #   key_name               = aws_key_pair.generated_key.key_name
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.generated_key.key_name
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  # user_data = <<-EOF
-  #               #!/bin/bash
-  #               apt-get update
-  #               apt-get install -y apache2
-  #               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-  #               echo "<h1>Fortune cookie app coming soon!</h1>" > /var/www/html/index.html
-  #               systemctl restart apache2
-  #               EOF
-  user_data = <<-EOF
-                #!/bin/bash
-                apt-get update
-                apt-get install -y git
-                git clone https://github.com/AlexSuspis/fortune-cookie-app.git
-                cd fortune-cookie-app
-                npm install
-                node app.js
-                EOF
 }
 
 
@@ -98,6 +80,7 @@ resource "aws_launch_template" "app" {
   name_prefix   = "app"
   image_id      = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.generated_key.key_name
   user_data     = filebase64("./ec2-setup.sh")
 }
 
