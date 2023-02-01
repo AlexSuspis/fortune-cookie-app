d313e570d6b3d92abb5e826928060aeddf9b78f9
Alex Suspiro <alexandresuspiro@gmail.com>
2023-02-01 07:22:47 +0000

    change ec2-setup.sh to create a very basic html server, as a proof of concept that Load Balancer works

--- ec2-setup.sh
+++ ec2-setup.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
-apt-get update
-apt-get install -y git
-git clone https://github.com/AlexSuspis/fortune-cookie-app.git
-cd fortune-cookie-app
-npm install
-node app.js
+# #!/bin/bash
+# apt-get update
+# apt-get install -y git
+# git clone https://github.com/AlexSuspis/fortune-cookie-app.git
+# cd fortune-cookie-app
+# npm install
+# node app.js
+sudo su
+yum -y install httpd
+echo "<p> My Instance! </p>" >> /var/www/html/index.html
+sudo systemctl enable httpd
+sudo systemctl start httpd
