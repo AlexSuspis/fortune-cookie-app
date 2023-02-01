8a30c2f725cc95ec538d9cfe2a2de12e0c6e367f
Alex Suspiro <alexandresuspiro@gmail.com>
2023-02-01 08:38:17 +0000

    change both ingress ports to zero so we can use the -1 protocol

--- main.tf
+++ main.tf
@@ -144,8 +144,8 @@ resource "aws_security_group" "elb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 8080
-    to_port     = 80
+    from_port   = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
