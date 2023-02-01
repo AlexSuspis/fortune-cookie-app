10a0bc686dfbd868329a9d48a68fb81b4434f839
Alex Suspiro <alexandresuspiro@gmail.com>
2023-02-01 08:02:24 +0000

    uncomment initial bash script line for web server setup in instances

--- ec2-setup.sh
+++ ec2-setup.sh
@@ -1,10 +1,10 @@
-# #!/bin/bash
 # apt-get update
 # apt-get install -y git
 # git clone https://github.com/AlexSuspis/fortune-cookie-app.git
 # cd fortune-cookie-app
 # npm install
 # node app.js
+#!/bin/bash
 sudo su
 yum -y install httpd
 echo "<p> My Instance! </p>" >> /var/www/html/index.html
