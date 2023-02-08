356aaaf41aaa12062f5471cf1a78638132eaa971
Alex Suspiro <alexandresuspiro@gmail.com>
2023-02-08 00:41:19 +0000

    add config parameters to Terraform file, as per https://github.com/hashicorp/terraform-provider-aws/issues/23167

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 3.74.2"
+    }
+  }
+}
 provider "aws" {
   region                  = "eu-west-2"
   profile                 = "new"
